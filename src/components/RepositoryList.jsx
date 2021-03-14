262382c6ce976e3e7f4cb865fad1911b2beb0dbe
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2021-03-14 20:26:15 -0300

    feat: fix props on React

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,35 +1,22 @@
+import { RepositoryItem } from './RepositoryItem';
+
+const repository = {
+  name: 'Test',
+  description: 'Test of props',
+  link: 'https://github.com/'
+}
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in ReactJS</p>
-
-          <a href="/">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in ReactJS</p>
-
-          <a href="/">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in ReactJS</p>
-
-          <a href="/">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
